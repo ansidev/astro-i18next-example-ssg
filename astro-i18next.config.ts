9d6e49ea5a34e527fb6d7840e597b2a6b62c3824
Le Minh Tri <ansidev@gmail.com>
2023-01-04 15:43:01 +0700

    feat(i18n-config): only enable debug mode if Node environment is 'development'

--- astro-i18next.config.ts
+++ astro-i18next.config.ts
@@ -4,7 +4,7 @@ const config: AstroI18nextConfig = {
   defaultLocale: "en",
   locales: ["en", "vi", "fr"],
   i18nextServer: {
-    debug: true,
+    debug: process.env.NODE_ENV !== 'development',
   },
 };
 
